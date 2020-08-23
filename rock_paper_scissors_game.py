5e21574ac6f7b3de3ec5fdcf45b4e13803f6948f
nitinsinghal94 <singhal.nitin94@gmail.com>
2020-08-23 19:41:49 +0000

    Added rock paper scissors game

---
+++ rock_paper_scissors_game.py
@@ -0,0 +1,66 @@
+# import modules
+import random, sys
+
+# print ROCK, PAPER, SCISSORS
+print('Welcome to ROCK, PAPER, SCISSORS Game!!!')
+wins = 0
+losses = 0
+ties = 0
+
+while True:
+    print('{} Wins, {} Losses, {} Ties'.format(wins, losses, ties))
+    while True:
+        print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
+        playerMove = input()
+        if playerMove == 'q':
+            sys.exit()
+        if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
+            break
+        print('Type one of r, p, s, or q.')
+    
+    # Display what the player chose:
+    if playerMove == 'r':
+        print('ROCK versus...')
+    elif playerMove == 'p':
+        print('PAPER versus...')
+    elif playerMove == 's':
+        print('SCISSORS versus...')
+
+    # Display what the computer chose:
+    randomNumber = random.randint(1, 3)
+    if randomNumber == 1:
+        computerMove = 'r'
+        print('ROCK')
+    elif randomNumber == 2:
+        computerMove = 'p'
+        print('PAPER')
+    elif randomNumber == 3:
+        computerMove = 's'
+        print('SCISSORS')
+
+    # Display and record the win/loss/tie:
+    if playerMove == computerMove:
+        print('It is a tie!')
+        ties = ties + 1
+    elif playerMove == 'r' and computerMove == 's':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'p' and computerMove == 'r':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 's' and computerMove == 'p':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'r' and computerMove == 'p':
+        print('You lose!')
+        losses = losses + 1
+    elif playerMove == 'p' and computerMove == 's':
+        print('You lose!')
+        losses = losses + 1
+    elif playerMove == 's' and computerMove == 'r':
+        print('You lose!')
+        losses = losses + 1
+
+
+
+    
