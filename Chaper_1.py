5027979a9e50e50ab5b5377e7f7fba8bb874a282
nitinsinghal94 <singhal.nitin94@gmail.com>
2020-08-23 19:08:00 +0000

    Added Chapter 1 code

--- Chaper_1.py
+++ Chaper_1.py
@@ -1,1 +1,15 @@
-print("Hello")
+# This program says hello and asks for my name and show my age.
+print('Hello, World!')
+
+print('What is your name?') #ask for name
+
+myName = input()
+
+print('It is good to meet you ' + myName)
+
+print('The length of your name is :')
+print(len(myName))
+
+print('Please tell your age:') # ask for age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1)+ ' in a year.')
